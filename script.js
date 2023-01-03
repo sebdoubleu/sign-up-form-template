408ec8b22f46863679227a54767ab511f8003017
sebdoubleu <111027905+sebdoubleu@users.noreply.github.com>
2023-01-03 10:34:11 -0500

    Add JS return alert to mock form submission

---
+++ script.js
@@ -0,0 +1,21 @@
+const button = document.querySelector('button');
+const inputs = document.querySelectorAll('input');
+
+button.addEventListener('click', () => {
+    let formData = {};
+    inputs.forEach(e => {
+        if (e.name == 'email' && !e.value.includes('@')) {
+            alert('Invalid email!');
+            return;
+        }
+        formData[e.name] = e.value;
+        e.value = '';
+    });
+    alert(`This is a test form. You submitted:
+    First Name: ${formData['first-name']}
+    Last Name: ${formData['last-name']}
+    Email: ${formData['email']}
+    Phone Number: ${formData['phone-number']}
+    Password: Hidden
+    Confirm Password: Hidden`);
+});
